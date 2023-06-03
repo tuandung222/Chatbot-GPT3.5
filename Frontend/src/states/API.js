570f6f162dcfcd097d4182a92b8465a6494ecc3c
TuanDung111 <75377334+tuandung222@users.noreply.github.com>
2023-06-03 23:59:18 +0700

    .

--- Frontend/src/states/API.js
+++ Frontend/src/states/API.js
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-console.log(import.meta.env);
 export const API = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.BACKEND_URL}),
+    baseQuery: fetchBaseQuery({ baseUrl: }),
     reducerPath: "main",
     tagTypes: [],
     endpoints: (build) => ({
