d11b4dc36ea6b17448879e2dda0a4689855e63b3
TuanDung111 <75377334+tuandung222@users.noreply.github.com>
2023-06-04 17:31:24 +0700

    update-frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Chat from './pages/Chat'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
