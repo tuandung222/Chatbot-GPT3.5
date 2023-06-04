a7ef14c6388dbf8af9afff2bc0ec3fd36276a7ef
TuanDung111 <75377334+tuandung222@users.noreply.github.com>
2023-06-04 17:46:22 +0700

    update-front-end

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MessageModule } from './message/message.module';
 import { LoginModule } from './login/login.module';
 import { SignupModule } from './signup/signup.module';
+import { MessageModule } from './message/message.module';
 
 
 @Module({
